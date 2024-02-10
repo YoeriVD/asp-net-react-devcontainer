0281a31d2fcbbb7d026c8827c09bb10ac7875731
yoerivd <yoeri.vandamme@gmail.com>
2024-02-10 12:13:06 +0000

    fix ssl settings localhost

--- react-app.client/vite.config.ts
+++ react-app.client/vite.config.ts
@@ -1,12 +1,12 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 import plugin from "@vitejs/plugin-react";
 import fs from "fs";
 import path from "path";
 import child_process from "child_process";
-const urls : string = process.env.ASPNETCORE_URLS;
-const sslEnabled = urls.indexOf('https') > -1;
+const urls: string = process.env.ASPNETCORE_URLS;
+const sslEnabled = urls.indexOf("https") > -1;
 
 const serverConfig = defineConfig({
   plugins: [plugin()],
@@ -18,85 +18,93 @@ const serverConfig = defineConfig({
   server: {
     proxy: {
       "^/weatherforecast": {
-        target: sslEnabled ? "https://localhost:7255/" : "http://localhost:5144",
+        target: "http://localhost:5144",
         secure: false,
       },
     },
     strictPort: true,
-    port: 5173
+    port: 5173,
   },
 });
 
-if(sslEnabled) {
+if (sslEnabled) {
   console.warn(`https url found in ${urls}, attempting vite with SSL!`);
-  try{
+  try {
     const sslSettings = createSslSettings();
-    defineConfig.server.https = sslSettings;
-  }
-  catch (ex) {
+    const sslServerConfig : Partial<UserConfig> = {
+      server: {
+        https: sslSettings,
+        proxy: {
+          "^/weatherforecast": {
+            target: "https://localhost:7255/",
+            secure: false,
+          },
+        },
+      },
+    };
+    Object.assign(serverConfig, sslServerConfig);
+  } catch (ex) {
     console.error(ex);
     process.exit(-1);
   }
-
 }
 
 // https://vitejs.dev/config/
 export default serverConfig;
 
-
 /**
  * Function to create SSL settings based on the ASPNETCORE developer cert (usefull for development on a local machine)
  * @returns ssl settings
  */
-function createSslSettings(){
+function createSslSettings() {
   const baseFolder =
-  process.env.APPDATA !== undefined && process.env.APPDATA !== ""
-    ? `${process.env.APPDATA}/ASP.NET/https`
-    : `${process.env.HOME}/.aspnet/https`;
-const pfx = "/https/aspnetapp.pfx";
+    process.env.APPDATA !== undefined && process.env.APPDATA !== ""
+      ? `${process.env.APPDATA}/ASP.NET/https`
+      : `${process.env.HOME}/.aspnet/https`;
+  const pfx = "/https/react-app.pfx";
 
-const certificateArg = process.argv
-  .map((arg) => arg.match(/--name=(?<value>.+)/i))
-  .filter(Boolean)[0];
-const certificateName = certificateArg
-  ? certificateArg.groups.value
-  : "react-app.client";
+  const certificateArg = process.argv
+    .map((arg) => arg.match(/--name=(?<value>.+)/i))
+    .filter(Boolean)[0];
+  const certificateName = certificateArg
+    ? certificateArg.groups.value
+    : "react-app.client";
 
-if (!certificateName) {
-  throw new Error(
-    "Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly."
-  );
-}
-
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
-
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-  if (
-    0 !==
-    child_process.spawnSync(
-      "dotnet",
-      [
-        "dev-certs",
-        "https",
-        "--export-path",
-        certFilePath,
-        "--format",
-        "Pem",
-        "--no-password",
-      ],
-      { stdio: "inherit" }
-    ).status
-  ) {
-    throw new Error("Could not create certificate.");
+  if (!certificateName) {
+    throw new Error(
+      "Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly."
+    );
   }
-}
 
-const https = fs.existsSync(pfx)
-  ? { pfx, passphrase: "password" }
-  : {
-      key: fs.readFileSync(keyFilePath),
-      cert: fs.readFileSync(certFilePath),
-    };
+  const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+  const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+
+  if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+    if (
+      0 !==
+      child_process.spawnSync(
+        "dotnet",
+        [
+          "dev-certs",
+          "https",
+          "--export-path",
+          certFilePath,
+          "--format",
+          "Pem",
+          "--no-password",
+        ],
+        { stdio: "inherit" }
+      ).status
+    ) {
+      throw new Error("Could not create certificate.");
+    }
+  }
+
+  const https = fs.existsSync(pfx)
+    ? { pfx, passphrase: "password" }
+    : {
+        key: fs.readFileSync(keyFilePath),
+        cert: fs.readFileSync(certFilePath),
+      };
   return https;
 }
