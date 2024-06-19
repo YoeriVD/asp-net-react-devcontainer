29845060f389e621a750b70a5be550fd37a3196b
yoerivd <yoeri.vandamme@gmail.com>
2024-06-19 10:59:41 +0200

    fix permissions

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,10 +1,6 @@
 bash .devcontainer/mssql/postCreateCommand.sh 'P@ssw0rd' './bin/Debug/' './.devcontainer/mssql/'
 
-echo "Setting permissions"
-# sudo chown vscode /home/vscode/.npm
-sudo chown vscode /workspaces/asp-net-react-devcontainer/react-app.client/node_modules
-sudo chown vscode /workspaces/asp-net-react-devcontainer/react-app.Server/bin
-sudo chown vscode /workspaces/asp-net-react-devcontainer/react-app.Server/obj
+
 
 echo "Restoring dotnet packages"
 dotnet restore
@@ -13,3 +9,10 @@ dotnet restore
 
 echo "Restoring npm packages"
 npm --prefix react-app.client install
+
+
+echo "Setting permissions"
+# sudo chown vscode /home/vscode/.npm
+sudo chown -R vscode /workspaces/asp-net-react-devcontainer/react-app.client/node_modules
+sudo chown -R vscode /workspaces/asp-net-react-devcontainer/react-app.Server/bin
+sudo chown -R vscode /workspaces/asp-net-react-devcontainer/react-app.Server/obj
